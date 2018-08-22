303f23ab14cb98fae39dc553cbc2f86ce1b7cf56
Chao <chaoaretasty@gmail.com>
2018-08-22 10:47:54 +0100

    Added Delimited reader and writer

---
+++ src/AddressProcessor/CSV/DelimitedReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AddressProcessing.CSV
+{
+	public class DelimitedReader : IDisposable
+	{
+		private readonly char[] _separator;
+		private readonly StreamReader _streamReader;
+		private readonly bool _endOnEmptyLine;
+
+		public DelimitedReader(string path) : this(path, true) { }
+		public DelimitedReader(string path, bool endOnEmptyLine) : this(path, endOnEmptyLine, new[] { '\t' }) { }
+		public DelimitedReader(string path, bool endOnEmptyLine, params char[] separator)
+		{
+			_endOnEmptyLine = endOnEmptyLine;
+			_streamReader = File.OpenText(path);
+			_separator = separator;
+		}
+
+		public IEnumerable<string> Read()
+		{
+			var line = _streamReader.ReadLine();
+			
+			if( line==null || _endOnEmptyLine && line.Length == 0)
+			{
+				return null;
+			}
+
+			return line.Split(_separator);
+		}
+
+		public bool TryRead(out IEnumerable<string> values)
+		{
+			values = Read();
+			return values != null;
+		}
+
+		public void Dispose()
+		{
+			_streamReader?.Dispose();
+		}
+	}
+}
---
+++ src/AddressProcessor/CSV/DelimitedWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressProcessing.CSV
+{
+	public class DelimitedWriter
+	{
+		private readonly char _separator;
+		private readonly StreamWriter _streamWriter;
+		private readonly bool _endOnEmptyLine;
+
+		public DelimitedWriter(string path) : this(path, false) { }
+		public DelimitedWriter(string path, bool append) : this(path, append, '\t') { }
+		public DelimitedWriter(string path, bool append, char separator)
+		{
+			var fileInfo = new FileInfo(path);
+			_streamWriter = append ? fileInfo.AppendText() : fileInfo.CreateText();
+			_separator = separator;
+		}
+
+		public void Write(params string[] columns) => Write(columns.AsEnumerable());
+		public void Write(IEnumerable<string> columns)
+		{
+			_streamWriter.WriteLine(String.Join(_separator.ToString(), columns));
+		}
+	}
+}
